fd2dc51a68f70e050d0ba4ad44b125a5c92c125a
Kay Wiberg <kaywib@gmail.com>
2024-02-06 15:18:42 +0100

    update .gitignore, README.md and main.py

--- README.md
+++ README.md
@@ -30,3 +30,69 @@ python main.py
 
 ### Generating Text
 After training or loading an existing model, you can generate text based on prompts by following the interactive command prompt instructions.
+
+
+## Tips and tricks to cut training, validation and test time
+
+### Model suggestions
+
+`distilgpt2` is a distilled version of GPT-2 that retains much of the original's capabilities but is smaller and faster to train:
+
+- distilgpt2
+- gpt2
+
+### Dataset suggestions
+
+- wikitext-2-v1 (44.8k rows)
+- wikitext-103-v1 (1.81 M rows)
+
+### Reduce Dataset Size
+
+Another way to reduce training time is by using a smaller portion of your dataset. Instead of training on the entire dataset, you can sample a subset of your data for training. This will reduce the amount of computation needed per epoch:
+
+```python
+from torch.utils.data import Subset
+import numpy as np
+
+# Assuming `train_dataset` is your full training dataset
+indices = np.random.choice(len(train_dataset), size=int(len(train_dataset) * 0.1), replace=False) # Sample 10% of the dataset
+small_train_dataset = Subset(train_dataset, indices)
+```
+Then, use small_train_dataset in your DataLoader instead of the full dataset.
+
+### Decrease the Number of Epochs
+
+Reducing the number of epochs will directly decrease training time. If you're just looking to learn the concepts, even a single epoch can be insightful:
+
+```python
+num_epochs = 1  # Reduce the number of epochs to 1 or a few
+
+```
+
+###  Increase the Learning Rate
+
+Sometimes, increasing the learning rate can help the model converge faster. However, this needs to be done carefully to avoid overshooting the minimum of the loss function. It's more of a trial-and-error process:
+
+```python
+optimizer = AdamW(model.parameters(), lr=1e-4)  # Adjust the learning rate as needed
+```
+
+### Utilize Gradient Accumulation
+
+If you're limited by the GPU memory and hence using a very small batch size, you can implement gradient accumulation to effectively increase the batch size without increasing the memory requirement. This method involves summing the gradients over several iterations and only updating the model weights after a specified number of steps. This can sometimes make training more efficient.
+
+
+
+
+
+### Test questions for the wikitext-103-v1 dataset
+
+- "In 2005, the Nobel Prize in Physics was awarded to John L. Hall and Theodor W. Hänsch for their work on precision spectroscopy and optical frequency combs."
+
+- "The Renaissance, a period of great cultural and artistic growth in Europe, began in the 14th century and continued into the 17th century."
+
+- "Marie Curie, a pioneering physicist and chemist, was the first woman to win a Nobel Prize and remains one of the most influential scientists in history."
+
+- "The Industrial Revolution, a transformative era in human history, saw the rapid development of machinery, factories, and urbanization."
+
+- "The theory of relativity, developed by Albert Einstein in the early 20th century, revolutionized our understanding of space, time, and gravity."
--- main.py
+++ main.py
@@ -1,20 +1,25 @@
-from datasets import load_dataset
 from transformers import GPT2Tokenizer, GPT2LMHeadModel
-from torch.optim import AdamW 
+from torch.optim import AdamW
 from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import Subset
+from tqdm import tqdm 
+import numpy as np
+import pandas as pd
 import torch
 import os
+
 
 # Constants for model and tokenizer paths
 MODEL_PATH = "./model/gpt2_finetuned_model"
 TOKENIZER_PATH = "./model/gpt2_finetuned_tokenizer"
+MODLE_NAME = 'distilgpt2'
 
 # Ensure CUDA is available and select the GPU
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(f"Using device: {device}")
 
 # Initialize tokenizer globally to ensure it's accessible throughout the script
-tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
+tokenizer = GPT2Tokenizer.from_pretrained(MODLE_NAME)
 tokenizer.pad_token = tokenizer.eos_token
 
 # Initialize model variable globally
@@ -22,41 +27,45 @@ model = None
 
 class WikiTextDataset(Dataset):
     def __init__(self, encodings):
+        # Assuming encodings is a dictionary with keys 'input_ids' and 'attention_mask'
+        # and their values are tensors.
         self.input_ids = encodings['input_ids']
         self.attention_mask = encodings['attention_mask']
 
     def __len__(self):
+        # Return the total number of items in the dataset
         return len(self.input_ids)
 
     def __getitem__(self, idx):
+        # Retrieve the item at the specified index.
+        # Use .clone().detach() to create a copy of the tensors that is not part of the
+        # original computation graph. This prevents unwanted side-effects during backpropagation.
         item = {
-            'input_ids': torch.tensor(self.input_ids[idx], dtype=torch.long),
-            'attention_mask': torch.tensor(self.attention_mask[idx], dtype=torch.long)
+            'input_ids': self.input_ids[idx].clone().detach(),
+            'attention_mask': self.attention_mask[idx].clone().detach()
         }
         return item
 
-def tokenize_function(examples):
-    # Use the globally defined tokenizer
-    return tokenizer(examples["text"], padding="max_length", truncation=True, max_length=512)
+
+def load_and_tokenize_dataset(file_path):
+    df = pd.read_parquet(file_path)
+    encodings = tokenizer(df['text'].tolist(), truncation=True, padding="max_length", max_length=512, return_tensors="pt")
+    return WikiTextDataset(encodings)
 
 def train_model():
-    global model  # Declare model as global to modify the global instance
-    print("Downloading and loading the dataset...")
-    # wikitext-103-v1
-    dataset = load_dataset('wikitext', 'wikitext-2-v1')
-    print("Dataset successfully downloaded and loaded.")
+    global model
+    print("Loading and tokenizing the training dataset...")
+    train_dataset = load_and_tokenize_dataset('./dataset/train-00000-of-00001.parquet')
     
-    print("Tokenizing the dataset. This might take a while...")
-    tokenized_datasets = dataset.map(tokenize_function, batched=True, remove_columns=["text"])
-    print("Tokenization complete.")
+    indices = np.random.choice(len(train_dataset), size=int(len(train_dataset) * 0.1), replace=False) # Sample 10% of the dataset
+    small_train_dataset = Subset(train_dataset, indices)
+    
+    print("Training dataset loaded and tokenized.")
 
-    train_dataset = WikiTextDataset(tokenized_datasets['train'])
-    print("Dataset prepared for training.")
-
-    dataloader = DataLoader(train_dataset, batch_size=16, shuffle=True)
+    dataloader = DataLoader(small_train_dataset, batch_size=16, shuffle=True)
     print("DataLoader initialized.")
 
-    model = GPT2LMHeadModel.from_pretrained("gpt2").to(device)
+    model = GPT2LMHeadModel.from_pretrained(MODLE_NAME).to(device)
     print("Model loaded and sent to the device.")
 
     optimizer = AdamW(model.parameters(), lr=5e-5)
@@ -67,7 +76,8 @@ def train_model():
     for epoch in range(num_epochs):
         print(f"Epoch {epoch + 1}/{num_epochs} started.")
         epoch_loss = 0.0
-        for batch in dataloader:
+        # Wrap dataloader with tqdm for a progress bar
+        for batch in tqdm(dataloader, desc=f"Training Epoch {epoch + 1}"):
             input_ids = batch['input_ids'].to(device)
             attention_mask = batch['attention_mask'].to(device)
             labels = input_ids.clone()
@@ -90,27 +100,59 @@ def train_model():
     tokenizer.save_pretrained(TOKENIZER_PATH)
     print("Model and tokenizer saved.")
 
+def validate_or_test_model(dataset_path, mode="Validation"):
+    global model
+    dataset = load_and_tokenize_dataset(dataset_path)
+    dataloader = DataLoader(dataset, batch_size=16)
+
+    print(f"{mode} started...")
+    model.eval()
+    total_loss = 0
+    # Wrap dataloader with tqdm for a progress bar
+    with torch.no_grad():
+        for batch in tqdm(dataloader, desc=f"{mode}"):
+            input_ids = batch['input_ids'].to(device)
+            attention_mask = batch['attention_mask'].to(device)
+            labels = input_ids.clone()
+
+            outputs = model(input_ids, attention_mask=attention_mask, labels=labels)
+            loss = outputs.loss
+            total_loss += loss.item()
+
+    avg_loss = total_loss / len(dataloader)
+    print(f"{mode} Loss: {avg_loss:.4f}")
+
 def check_and_load_model():
-    global model, tokenizer  # Declare as global to modify the global instance
+    global model, tokenizer
     if os.path.exists(MODEL_PATH) and os.path.exists(TOKENIZER_PATH):
-        print(f"A trained model already exists. Loading model and tokenizer.")
+        print("A trained model already exists. Loading model and tokenizer.")
+        model = GPT2LMHeadModel.from_pretrained(MODEL_PATH).to(device)
         tokenizer = GPT2Tokenizer.from_pretrained(TOKENIZER_PATH)
-        model = GPT2LMHeadModel.from_pretrained(MODEL_PATH)
-        model.to(device)
         print("Loaded model and tokenizer from saved files.")
     else:
-        print(f"A trained model could not be found, Initiating training of GPT-2 model.")
+        print("A trained model could not be found. Initiating training of GPT-2 model.")
         train_model()
+        validate_or_test_model('./dataset/validation-00000-of-00001.parquet', mode="Validation")
+        validate_or_test_model('./dataset/test-00000-of-00001.parquet', mode="Test")
 
 def generate_text(prompt, max_length=50, temperature=1.0, num_return_sequences=1):
-    global model  # Ensure model is recognized within this function
-    print(f"Generating text for prompt: '{prompt}'")
-    model.eval()  # Set model to evaluation mode
+    global model
+    print(f"Generating answer for prompt: '{prompt}'")
+    print("Generating text:")
+    model.eval()
 
     encoded_prompt = tokenizer.encode(prompt, add_special_tokens=False, return_tensors="pt").to(device)
 
+    # Ensure the attention mask is set to 1 for all input tokens
+    attention_mask = torch.ones_like(encoded_prompt).to(device)
+
+    # Set the pad token ID to EOS token ID
+    pad_token_id = tokenizer.eos_token_id
+
     output_sequences = model.generate(
         input_ids=encoded_prompt,
+        attention_mask=attention_mask,  # Set attention mask
+        pad_token_id=pad_token_id,      # Set pad token ID
         max_length=max_length + len(encoded_prompt[0]),
         temperature=temperature,
         top_k=20,
@@ -122,17 +164,26 @@ def generate_text(prompt, max_length=50, temperature=1.0, num_return_sequences=1
 
     for i, generated_sequence in enumerate(output_sequences):
         text = tokenizer.decode(generated_sequence, clean_up_tokenization_spaces=True)
-        print(f"Generated Text {i+1}: {text}")
+        print(f"\nGenerated Text {i+1}:")
+        print(text)
 
 def main():
-    choice = input("Enter your choice (1: Load Model, 2: Generate Text): ")
-    if choice == "1":
-        check_and_load_model()
-    elif choice == "2":
-        prompt = input("Enter the prompt: ")
-        generate_text(prompt, max_length=50, temperature=0.7, num_return_sequences=3)
-    else:
-        print("Invalid choice.")
+    while True:
+        choice = input("Enter your choice (1: Load Model, 2: Generate Text, q: Quit): ")
+        if choice == "1":
+            check_and_load_model()
+            # Wait for user input before proceeding
+            input("\nModel has been loaded. Press Enter to continue...")
+        elif choice == "2":
+            if model is None:
+                print("\nModel has not been loaded. Please choose option 1 to load the model first.")
+            else:
+                prompt = input("\nEnter the prompt: ")
+                generate_text(prompt, max_length=50, temperature=0.7, num_return_sequences=3)
+        elif choice.lower() == "q":
+            break  # Exit the loop if 'q' is chosen
+        else:
+            print("\nInvalid choice. Please choose a valid option (1, 2, or q).")
 
 if __name__ == "__main__":
     main()
